2f9ac5033421524eb1c9f2fba3b43383b40246a8
nir <nirhamer17@walla.co.il>
2019-12-04 18:12:29 +0200

    Add solution: filter and sort pairs

--- Map/Programming (5)/src/Main.java
+++ Map/Programming (5)/src/Main.java
@@ -1,16 +1,53 @@
-import jdk.internal.net.http.common.Pair;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Object b;
-    Pair p1 = new Pair(2,b);
-    Object c;
-    Pair p2 = new Pair(3,c);
-    Object d;
-    Pair p3 = new Pair(4,d);
-    System.out.println(p1.equals(p2) + “ ” + p2.equals(p3);
+  /*
+  ranges
+   */
+        int from;
+        int to;
 
-  }
+  /*
+  data count
+   */
+        int pairsCount;
+
+    /*
+  key-value pairs
+   */
+        Map<Integer, String> pairs = new TreeMap<>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        from = scanner.nextInt();
+        to = scanner.nextInt();
+
+
+        pairsCount = scanner.nextInt();
+
+//        System.out.println(String.format("from: %d, to: %d, count: %d",
+//                from, to, pairsCount));
+//        System.out.println("Please enter pairs:");
+
+
+        for (int i = 0; i < pairsCount; i++) {
+            pairs.put(scanner.nextInt(), scanner.nextLine().trim());
+
+        }
+
+
+        for (Map.Entry<Integer, String> pair : pairs.entrySet()) {
+            Integer thisKey = pair.getKey();
+            if (thisKey >= from && thisKey <= to) {
+                System.out.println(String.format("%d %s", thisKey, pair.getValue()));
+            }
+            //ctlrl - alt  - l => rearrange all code
+        }
+    }
 }
